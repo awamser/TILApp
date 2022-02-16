7ca582430c0203408a69f2735a13d626f142d5d9
Alan Wamser <alan@wamser.com>
2022-02-15 22:44:52 -0600

    added views

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,8 +6,8 @@ import Leaf
 // configures your application
 public func configure(_ app: Application) throws {
   // uncomment to serve files from /Public folder
-  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-  
+  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
   // added for unit testing
   let databaseName: String
   let databasePort: Int
